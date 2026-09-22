f59516351200d9b5088634e133280ed7f1019e1c
ali-hassnain-se <ranaalihassnain07@gmail.com>
2026-09-22 23:22:33 +0500

    Added basics of python

---
+++ basics.py
@@ -0,0 +1,40 @@
+# OUTPUT
+# print("Hello", "World!")
+# # Here if we use comma(,) it will seperate strings otherwise print them combines
+
+""" 
+VARIABLES
+Variable: variable is a named storage location that is used to store value
+"""
+# name="Rana Ali Hassnain" # string
+# age=19 # integer
+# cgpa=3.9 # float
+# is_Student=True # boolean
+# print(name, age, cgpa, is_Student)
+
+# # print data type of variables
+# print(type(name))
+
+# INPU#T
+# input("Enter Your Name: ") # first method to take input
+
+name=input("Enter Your Name: ") # it will be stored in name variable
+
+# print("Hello", name)
+print("Hello " + name) # second way to print
+# Concatination: when we add two different strings
+
+"""
+COMMENTS 
+Sigle Line Comment=[#] & Multi-Line Comments=[""" """]
+comments are those part of our code that's ignored by our compiler/interpreter
+if we want to inform that what's actually running in our programwe can use them
+"""
+
+# PRACTICE EXCERCISE 01
+name="Tony Stark"
+age=53
+height=1.85
+
+s_name=input("Enter Your Super Hero Name: ")
+print(s_name)
